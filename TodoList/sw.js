d753285ee6a7682d7e2f3f804ab1a9e05d17d206
andrew <andrew@example.com>
2017-07-31 18:53:08 +0300

    start working with Service Workers

--- TodoList/sw.js
+++ TodoList/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'my-site-cache-v1';
-const urlsToCache = [
-  '/',
-  '/TodoList/dist/bundle.css',
-  '/TodoList/dist/bundle.js'
-];
+// let CACHE_NAME = 'my-site-cache-v1';
+// let urlsToCache = [
+//   '/',
+//   '/TodoList/dist/bundle.css',
+//   '/TodoList/dist/bundle.js'
+// ];
 
 // self.addEventListener('install', function(event) {
 //   // установка
@@ -18,8 +18,11 @@ const urlsToCache = [
 
 this.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/TodoList/dist/bundle.css',
+        '/TodoList/dist/bundle.js',
+      ]);
     })
   );
 });
