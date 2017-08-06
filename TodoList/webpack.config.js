6c431a098755cef4eac8e57a32832e0be95d5894
andrew <andrew@example.com>
2017-08-06 23:32:01 +0300

    convert js in ts

--- TodoList/webpack.config.js
+++ TodoList/webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
 	output:{
 		path: __dirname + "/dist/",
         filename: "[name].js",
-        chunkFilename: 'dist/[name].js'
+        publicPath: '.',
+        chunkFilename: '[name].js'
 	},
 
     devtool: 'inline-source-map',
